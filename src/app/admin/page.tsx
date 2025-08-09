0fc3c965df09abe506a3b271cbc6fb3b0b202d26
Kevin Eloy miani <kevinemiani@gmail.com>
2025-08-09 17:26:11 -0300

    admin ux/ui2

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 // src/app/admin/page.tsx
 'use client';
-import './admin-ui-pro.css';
+import './admin.css';
 import AdminPage from './AdminPage';
 
 export default function Page(){ return <AdminPage/> }
